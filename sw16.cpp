15b72ffd2faed116a2259c0e07cfb797890b212d
2neko9 <137231940+2neko9@users.noreply.github.com>
2023-09-26 21:55:52 +0800

    Update sw16.cpp

--- sw16.cpp
+++ sw16.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main()
 {
     int sum;
-    cout<<"This program will compute and display the sum and the average of 615,512 and 48.\n\n";
+    cout<<"This program will compute and display the sum and the average of 615,512 and 48.";
     sum = 615 + 512 + 48;
-    cout<<"The sum is: "<<sum;
     float average = sum / 3;
+    cout<<"\n\nThe sum is: "<<sum;
     cout<<"\nThe average is: "<<average;
 
     return 0;
