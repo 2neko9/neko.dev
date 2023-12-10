864d3141c2b521b29107ab49d5c5ce5ca74392c0
2neko9 <137231940+2neko9@users.noreply.github.com>
2023-12-10 23:41:55 +0800

    Update sw21.cpp

--- sw21.cpp
+++ sw21.cpp
@@ -9,44 +9,43 @@ int main()
     double cur1,prev1, curbill, total;
     char op;
 
-    string choice[3] = {"","[1] Residential cost P20.00", "[2] Commercial cost P30.00"};
+    cout<<"SeatNumber: 31";
+    cout<<"\nSeatWork number: 21";
+    cout<<"\n\n\t\t\tWater Bill Calculator\n\n";
+    cout<<"[1] Residential cost P20.00\n[2] Commercial cost P30.00\n\n";
 
-    cout<<"Select whether: Residential or Commercial\n\n";
-    for( i = 1; i < 3; i++){
-                cout<<choice[i]<<"\n";
-                    }
-    cout<<"\nEnter what number: ";
-    cin>>op;
-    cout<<"\n\nAccount Classification\n\n";
+    cout<<"Account Classification::\n\n";
     cout<<"What is your current water consumption in cubic meter: ";
     cin>>cur1;
     cout<<"What is your previous water consumption in cubic meter: ";
     cin>>prev1;
 
-    switch (op) 
-    {
-        case '1':;
-            if(prev1 > cur1){
+    if(prev1 >= cur1){
                 curbill = prev1 - cur1;
-            } else{
+            } 
+            else
+            {
                 curbill = cur1 - prev1;
             }
-        total = 20.00 * curbill;
-        cout<<"\n\nThe total bill is : "<< total << " pesos";
-        
-        break;
-    }
+
+    cout<<"\n\nSelect whether: Residential or Commercial\n\n";
+    cout<<"\nEnter what number: ";
+    cin>>op;
+
     switch (op) 
     {
-        case '2':;
-            if(prev1 > cur1){
-                curbill = prev1 - cur1;
-            } else{
-                curbill = cur1 - prev1;
-            }
-        total = 30.00 * curbill;
-        cout<<"\n\nThe total bill is: "<< total << " pesos";
+        case '1':
+            total = 20.00 * curbill;
+            cout<<"\n\nThe total bill is: "<< total << " pesos";
+            break;
         
+        case '2':
+            total = 30.00 * curbill;
+            cout<<"\n\nThe total bill is: "<< total << " pesos";
+            break;
+        
+        default:
+        cout<<"\nInvalid input!!!\n";
         break;
     }
     return 0;
